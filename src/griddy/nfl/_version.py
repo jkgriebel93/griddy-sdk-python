d04a7509ff1513f6c037fd3b31546aa16fb6be51
jkgriebel93 <jkgriebel93@users.noreply.github.com>
2025-10-10 13:11:44 +0000

    chore: Update SDK from spec changes
    Source commit: a1d1044
    Source message: Always build on push to master

--- src/griddy/nfl/_version.py
+++ src/griddy/nfl/_version.py
@@ -3,10 +3,10 @@
 import importlib.metadata
 
 __title__: str = "griddy-nfl"
-__version__: str = "0.1.3"
+__version__: str = "0.1.0"
 __openapi_doc_version__: str = "1.0.0"
-__gen_version__: str = "2.721.3"
-__user_agent__: str = "speakeasy-sdk/python 0.1.3 2.721.3 1.0.0 griddy-nfl"
+__gen_version__: str = "2.723.11"
+__user_agent__: str = "speakeasy-sdk/python 0.1.0 2.723.11 1.0.0 griddy-nfl"
 
 try:
     if __package__ is not None:
