4543ed07bb88684899728f5202b7988815fac6e1
John Griebel <jkgriebel93@gmail.com>
2025-10-17 06:53:00 -0400

    Pro APIs seem to be working as well
    Signed-off-by: John Griebel <jkgriebel93@gmail.com>

--- griddy_test.py
+++ griddy_test.py
@@ -9,6 +9,8 @@ config = NFLConfiguration(access_token=bearer_token)
 
 with NFLAPIClient(configuration=config) as api_client:
     api_instance = nfl.FootballController(api_client=api_client)
+    player_stats = nfl.PlayerStatisticsController(api_client=api_client)
+
     season = 2025
     type_ = nfl.SeasonTypeEnum.REG
     week = 7
@@ -17,8 +19,15 @@ with NFLAPIClient(configuration=config) as api_client:
         response = api_instance.get_weekly_game_details(season=season,
                                                         type=type_,
                                                         week=week)
+        stats_response = player_stats.get_player_passing_stats_by_season(season=season,
+                                                                         season_type=type_)
+
         print("API Response:\n\n\n")
         from pprint import pprint
-        pprint(response, indent=4)
+        pprint(stats_response.model_dump(), indent=4)
+
     except Exception as e:
         raise e
+
+
+
