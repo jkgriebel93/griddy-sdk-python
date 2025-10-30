eb9e360b3a3f5c1506d372b0bc3cc2741c0c5849
John Griebel <jkgriebel93@gmail.com>
2025-10-30 11:11:47 -0400

    [SDK #64] * Move existing pro team functionality to endpoints module
    Signed-off-by: John Griebel <jkgriebel93@gmail.com>

--- griddy_test.py
+++ griddy_test.py
@@ -5,7 +5,6 @@ from griddy.nfl import GriddyNFL
 # Lamar Jackson = 46101
 
 nfl = GriddyNFL("cookies.txt")
-player_response = nfl.players.get_player(nfl_id=46101)
-# pprint(player_response, indent=4)
-search_resp = nfl.players.search_players(term="Rodgers")
-pprint(search_resp.players[0].model_dump(), indent=4)
+all_teams_response = nfl.teams.get_all_teams()
+example_team = all_teams_response[0]
+pprint(example_team.model_dump(), indent=4)
