d9ed95c71c89f764e391d7ab1d6ae7b0bd339d7a
John Griebel <jkgriebel93@gmail.com>
2025-12-01 17:16:04 -0500

    [SDK #103] * Add missing Pydantic models
    Signed-off-by: John Griebel <jkgriebel93@gmail.com>

---
+++ src/griddy/nfl/models/responses/ngs_news_response.py
@@ -0,0 +1,66 @@
+"""Response models for NGS news endpoints (articles, videos, mixed content)."""
+
+from __future__ import annotations
+
+from typing import List, Optional
+
+import pydantic
+from typing_extensions import Annotated, NotRequired, TypedDict
+
+from griddy.nfl.models.entities.ngs_news import (
+    NgsContentItem,
+    NgsContentItemTypedDict,
+    NgsContentMetadata,
+    NgsContentMetadataTypedDict,
+    NgsContentPagination,
+    NgsContentPaginationTypedDict,
+)
+from griddy.nfl.types import BaseModel
+
+
+class NgsMixedContentResponseTypedDict(TypedDict):
+    """Response from the NGS mixed content endpoint."""
+
+    items: NotRequired[List[NgsContentItemTypedDict]]
+    metadata: NotRequired[NgsContentMetadataTypedDict]
+    pagination: NotRequired[NgsContentPaginationTypedDict]
+
+
+class NgsMixedContentResponse(BaseModel):
+    """Response from the NGS mixed content endpoint."""
+
+    items: Optional[List[NgsContentItem]] = None
+    metadata: Optional[NgsContentMetadata] = None
+    pagination: Optional[NgsContentPagination] = None
+
+
+class NgsArticlesResponseTypedDict(TypedDict):
+    """Response from the NGS articles endpoint."""
+
+    items: NotRequired[List[NgsContentItemTypedDict]]
+    metadata: NotRequired[NgsContentMetadataTypedDict]
+    pagination: NotRequired[NgsContentPaginationTypedDict]
+
+
+class NgsArticlesResponse(BaseModel):
+    """Response from the NGS articles endpoint."""
+
+    items: Optional[List[NgsContentItem]] = None
+    metadata: Optional[NgsContentMetadata] = None
+    pagination: Optional[NgsContentPagination] = None
+
+
+class NgsVideosResponseTypedDict(TypedDict):
+    """Response from the NGS videos endpoint."""
+
+    items: NotRequired[List[NgsContentItemTypedDict]]
+    metadata: NotRequired[NgsContentMetadataTypedDict]
+    pagination: NotRequired[NgsContentPaginationTypedDict]
+
+
+class NgsVideosResponse(BaseModel):
+    """Response from the NGS videos endpoint."""
+
+    items: Optional[List[NgsContentItem]] = None
+    metadata: Optional[NgsContentMetadata] = None
+    pagination: Optional[NgsContentPagination] = None
---
+++ src/griddy/nfl/models/responses/ngs_stats_response.py
@@ -0,0 +1,76 @@
+"""Response models for NGS stats endpoints (passing, receiving, rushing)."""
+
+from __future__ import annotations
+
+from typing import List, Optional
+
+import pydantic
+from typing_extensions import Annotated, NotRequired, TypedDict
+
+from griddy.nfl.models.entities.ngs_stats import (
+    NgsPassingStat,
+    NgsPassingStatTypedDict,
+    NgsReceivingStat,
+    NgsReceivingStatTypedDict,
+    NgsRushingStat,
+    NgsRushingStatTypedDict,
+)
+from griddy.nfl.types import BaseModel
+
+
+class NgsPassingStatsResponseTypedDict(TypedDict):
+    """Response from the NGS passing stats endpoint."""
+
+    season: NotRequired[int]
+    season_type: NotRequired[str]
+    filter: NotRequired[str]
+    threshold: NotRequired[int]
+    stats: NotRequired[List[NgsPassingStatTypedDict]]
+
+
+class NgsPassingStatsResponse(BaseModel):
+    """Response from the NGS passing stats endpoint."""
+
+    season: Optional[int] = None
+    season_type: Annotated[Optional[str], pydantic.Field(alias="seasonType")] = None
+    filter: Optional[str] = None
+    threshold: Optional[int] = None
+    stats: Optional[List[NgsPassingStat]] = None
+
+
+class NgsReceivingStatsResponseTypedDict(TypedDict):
+    """Response from the NGS receiving stats endpoint."""
+
+    season: NotRequired[int]
+    season_type: NotRequired[str]
+    threshold: NotRequired[int]
+    stats: NotRequired[List[NgsReceivingStatTypedDict]]
+
+
+class NgsReceivingStatsResponse(BaseModel):
+    """Response from the NGS receiving stats endpoint."""
+
+    season: Optional[int] = None
+    season_type: Annotated[Optional[str], pydantic.Field(alias="seasonType")] = None
+    threshold: Optional[int] = None
+    stats: Optional[List[NgsReceivingStat]] = None
+
+
+class NgsRushingStatsResponseTypedDict(TypedDict):
+    """Response from the NGS rushing stats endpoint."""
+
+    season: NotRequired[int]
+    season_type: NotRequired[str]
+    filter: NotRequired[str]
+    threshold: NotRequired[int]
+    stats: NotRequired[List[NgsRushingStatTypedDict]]
+
+
+class NgsRushingStatsResponse(BaseModel):
+    """Response from the NGS rushing stats endpoint."""
+
+    season: Optional[int] = None
+    season_type: Annotated[Optional[str], pydantic.Field(alias="seasonType")] = None
+    filter: Optional[str] = None
+    threshold: Optional[int] = None
+    stats: Optional[List[NgsRushingStat]] = None
