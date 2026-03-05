dbc87c56551fa645d0c12baa4eabd21a4a331e6c
John Griebel <john@thistlegrow.com>
2026-03-05 14:01:52 -0500

    chore(TGF-145): replace wildcard imports with explicit imports and __all__ in nfl and pfr __init__.py
    Signed-off-by: John Griebel <john@thistlegrow.com>

--- src/griddy/nfl/__init__.py
+++ src/griddy/nfl/__init__.py
@@ -2,8 +2,16 @@ from ._version import (
     __user_agent__,
     __version__,
 )
-from .sdk import *  # noqa: F401,F403
-from .sdkconfiguration import *  # noqa: F401,F403
+from .sdk import GriddyNFL
+from .sdkconfiguration import SERVERS, SDKConfiguration
+
+__all__ = [
+    "GriddyNFL",
+    "SDKConfiguration",
+    "SERVERS",
+    "VERSION",
+    "USER_AGENT",
+]
 
 VERSION: str = __version__
 USER_AGENT = __user_agent__
--- src/griddy/pfr/__init__.py
+++ src/griddy/pfr/__init__.py
@@ -1,6 +1,14 @@
 from ._version import __user_agent__, __version__
-from .sdk import *  # noqa: F401,F403
-from .sdkconfiguration import *  # noqa: F401,F403
+from .sdk import GriddyPFR
+from .sdkconfiguration import SERVERS, SDKConfiguration
+
+__all__ = [
+    "GriddyPFR",
+    "SDKConfiguration",
+    "SERVERS",
+    "VERSION",
+    "USER_AGENT",
+]
 
 VERSION: str = __version__
 USER_AGENT: str = __user_agent__
